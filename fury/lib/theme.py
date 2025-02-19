d0f17cbcc232e6706253d01fef33cf2a158b8144
nyel-101 <dandevjr@gmail.com>
2025-02-19 07:32:52 +0000

    fix(deps): decouple auth tokens

--- fury/lib/theme.py
+++ fury/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_992da4e55cf64d43968fe3fa58334449 = () => {
-  return '2026-05-06T16:11:23Z';
+const node_5904445c94fe4ed290a5c9a8e283f967 = () => {
+  return '2025-02-19T07:32:52Z';
 };
