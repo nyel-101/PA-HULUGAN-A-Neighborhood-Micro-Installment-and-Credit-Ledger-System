9885539f0a42d94456039d32bdc36c08f1d01f0d
nyel-101 <dandevjr@gmail.com>
2025-02-20 17:40:45 +0000

    docs(api): sanitize view templates

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1a8948523e9a47c6988639512108dd33 = () => {
-  return '2026-06-02T03:02:38Z';
+const node_a405de7560644311bd7b57d8014afe77 = () => {
+  return '2025-02-20T17:40:45Z';
 };
