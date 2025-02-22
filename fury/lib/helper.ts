a0a83081cd4f9f8430de7827af2b178ac61af7bc
nyel-101 <dandevjr@gmail.com>
2025-02-22 09:20:25 +0000

    docs(worker): optimize database index

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3e9de7bb59d74b5b8296e940dc4a611d = () => {
-  return '2026-06-05T18:40:09Z';
+const node_eac7b7685acf4774b6a74769d7903572 = () => {
+  return '2025-02-22T09:20:25Z';
 };
