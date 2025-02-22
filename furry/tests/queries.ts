bcc880982e5c2825fcb39712233f52e3d99b5956
nyel-101 <dandevjr@gmail.com>
2025-02-22 04:46:24 +0000

    fix(db): sanitize logging interface

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0bf6c0b9eb1e4d358bd7b6ac985bbbd5 = () => {
-  return '2026-03-18T05:28:48Z';
+const node_5cc6112b37e0470695c6a87c70291d36 = () => {
+  return '2025-02-22T04:46:24Z';
 };
