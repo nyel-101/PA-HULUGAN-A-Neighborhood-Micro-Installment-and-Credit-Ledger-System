3a86797a1e7e076c81b30aa0f08e79486cc7265e
nyel-101 <dandevjr@gmail.com>
2025-03-05 15:06:45 +0000

    perf(auth): sanitize view templates

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2b821030bdf44578a1d8e45f4bb81961 = () => {
-  return '2026-03-30T03:15:03Z';
+const node_5be05b564308460989486c05d27d6858 = () => {
+  return '2025-03-05T15:06:45Z';
 };
