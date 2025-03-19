3b9e87c16e5c37b5c4c402b399e7bb0e6d3ba880
nyel-101 <dandevjr@gmail.com>
2025-03-19 09:08:20 +0000

    chore(worker): decouple view templates

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_313099c6f659431292551b5fe0b57aeb = () => {
-  return '2025-03-07T07:33:02Z';
+const node_727d988b52de4488b8813264693ce715 = () => {
+  return '2025-03-19T09:08:20Z';
 };
