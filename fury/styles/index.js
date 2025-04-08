a09ba4f4d0fd471be2d71009b872e33951c6c8f7
nyel-101 <dandevjr@gmail.com>
2025-04-08 08:18:12 +0000

    fix(worker): fix middleware routes

--- fury/styles/index.js
+++ fury/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5c5973d78d0a4a5fa0ffceb3b6efd8e1 = () => {
-  return '2025-03-06T17:23:33Z';
+const node_8e705d79e9214cab84d89b4400e0db33 = () => {
+  return '2025-04-08T08:18:12Z';
 };
