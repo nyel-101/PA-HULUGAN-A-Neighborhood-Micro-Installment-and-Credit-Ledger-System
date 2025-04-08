4a4ceadd335ced5b79f77309251405367a568a7b
nyel-101 <dandevjr@gmail.com>
2025-04-08 16:53:37 +0000

    feat(db): restructure database index

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_73a5562c62dc469aaa19d50feeb71d06 = () => {
-  return '2025-03-21T18:06:06Z';
+const node_b1f087367f9346f6adfd397a242d2f17 = () => {
+  return '2025-04-08T16:53:37Z';
 };
