c89dc4c454a9dce2c29ef4a2779a1f6af67974dd
nyel-101 <dandevjr@gmail.com>
2025-04-09 12:03:03 +0000

    feat(worker): fix database index

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_75de5d95ccec4dd081cab76ff9f69806 = () => {
-  return '2026-02-10T14:30:12Z';
+const node_127675381c2f4f839740aee7e7664493 = () => {
+  return '2025-04-09T12:03:03Z';
 };
