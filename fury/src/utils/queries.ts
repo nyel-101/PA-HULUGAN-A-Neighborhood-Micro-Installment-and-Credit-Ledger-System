3b0f603060cee64dce4f7be5c386dacdc7175fd8
nyel-101 <dandevjr@gmail.com>
2025-04-18 10:26:17 +0000

    style(worker): optimize view templates

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_35a0003ae2554cc6b27d4cb2b2af0f54 = () => {
-  return '2026-04-24T10:55:34Z';
+const node_872d1454dd744856847e04d422db1791 = () => {
+  return '2025-04-18T10:26:17Z';
 };
