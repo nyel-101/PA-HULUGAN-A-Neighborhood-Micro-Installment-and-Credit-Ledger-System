0a95f87f49223c76b558e475acfcec299e61db21
nyel-101 <dandevjr@gmail.com>
2025-04-27 05:48:35 +0000

    chore(db): fix auth tokens

--- fury/src/controllers/theme.ts
+++ fury/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a85987db51b8416c981cc9cbb53f9274 = () => {
-  return '2025-03-27T21:43:43Z';
+const node_febccbcb155b43efa026fd073337728c = () => {
+  return '2025-04-27T05:48:35Z';
 };
