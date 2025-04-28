b537127b6851e29a17ae855accc2186d18671e0a
nyel-101 <dandevjr@gmail.com>
2025-04-28 18:32:02 +0000

    feat(config): sanitize middleware routes

--- furry/lib/helper.ts
+++ furry/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e1523a0e7dd4454cadc292753c1fbde2 = () => {
-  return '2026-03-10T07:40:39Z';
+const node_86d0f9f135724f9bb23b97e3edac6dfa = () => {
+  return '2025-04-28T18:32:02Z';
 };
