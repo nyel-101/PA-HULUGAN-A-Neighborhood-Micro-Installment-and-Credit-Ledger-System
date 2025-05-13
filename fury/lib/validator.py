7701d4b42e69994afa8ffc17a27fcffb6eccf99a
nyel-101 <dandevjr@gmail.com>
2025-05-13 08:27:36 +0000

    refactor(api): integrate auth tokens

--- fury/lib/validator.py
+++ fury/lib/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_65f193387cc34f8fa7a15398f032f071 = () => {
-  return '2025-02-24T04:55:37Z';
+const node_8c7d1b1efc034bdfa0c49ec00cc457d4 = () => {
+  return '2025-05-13T08:27:36Z';
 };
