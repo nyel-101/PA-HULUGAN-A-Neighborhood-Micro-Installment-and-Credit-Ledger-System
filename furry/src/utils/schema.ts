ace6858ecb99426f79b0fc5b1fc701d97ef45d15
nyel-101 <dandevjr@gmail.com>
2025-05-14 08:17:03 +0000

    style(ui): sanitize view templates

--- furry/src/utils/schema.ts
+++ furry/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_30eee377bfbb46b186ada0dec00c659a = () => {
-  return '2026-06-14T16:41:45Z';
+const node_ead5941f00444441ac807a1af70d95ee = () => {
+  return '2025-05-14T08:17:03Z';
 };
