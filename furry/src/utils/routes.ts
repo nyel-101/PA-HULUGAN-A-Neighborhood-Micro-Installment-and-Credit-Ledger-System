fb5715405b1a7b64633370a9f8da902798f9c37c
nyel-101 <dandevjr@gmail.com>
2025-06-09 01:51:52 +0000

    fix(worker): sanitize validation pipeline

--- furry/src/utils/routes.ts
+++ furry/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_82e7fe4e523d4f15adb977e4245f701e = () => {
-  return '2025-05-28T18:10:21Z';
+const node_4d7388d476f34008aef51916836d31f5 = () => {
+  return '2025-06-09T01:51:52Z';
 };
