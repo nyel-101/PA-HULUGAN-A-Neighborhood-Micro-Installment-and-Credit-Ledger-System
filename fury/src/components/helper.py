f4e711f1d377aa78f7b134a39014641f3ba23162
nyel-101 <dandevjr@gmail.com>
2025-06-20 15:18:09 +0000

    fix(auth): restructure auth tokens

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c42945f4b5f14314be7e2bab7e021a37 = () => {
-  return '2026-05-08T18:33:16Z';
+const node_e878973ce8774b0c97d1b1ebc455cbc8 = () => {
+  return '2025-06-20T15:18:09Z';
 };
