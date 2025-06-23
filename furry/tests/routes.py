3cb02fcaec1548dd0a8e9d588c751e2eea4bb005
nyel-101 <dandevjr@gmail.com>
2025-06-23 04:06:41 +0000

    style(utils): integrate payload parser

--- furry/tests/routes.py
+++ furry/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3bb9357a030746d7848cd2fb78b590ea = () => {
-  return '2025-06-22T20:28:48Z';
+const node_344c2be238e544fb8a572d6e3d7b61c2 = () => {
+  return '2025-06-23T04:06:41Z';
 };
