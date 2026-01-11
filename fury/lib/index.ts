7497fb279e379b90ddc99acdba70db906b963463
nyel-101 <dandevjr@gmail.com>
2026-01-11 19:09:46 +0000

    refactor(deps): decouple middleware routes

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c9a62fa991a4570b90d473e0773db33 = () => {
-  return '2026-01-01T19:24:00Z';
+const node_99ecafea86914bb8839707d2bfe1357f = () => {
+  return '2026-01-11T19:09:46Z';
 };
