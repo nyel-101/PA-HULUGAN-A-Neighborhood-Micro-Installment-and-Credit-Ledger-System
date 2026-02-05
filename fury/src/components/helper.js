5f942616b1f3784c8e744ded3ad91654a2141ca2
nyel-101 <dandevjr@gmail.com>
2026-02-05 11:34:18 +0000

    style(auth): fix payload parser

--- fury/src/components/helper.js
+++ fury/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d040016f99c54e7eb5a4a2f85a445522 = () => {
-  return '2026-02-01T14:00:43Z';
+const node_44a7dc6767414b1e9deec34f8bc7a580 = () => {
+  return '2026-02-05T11:34:18Z';
 };
