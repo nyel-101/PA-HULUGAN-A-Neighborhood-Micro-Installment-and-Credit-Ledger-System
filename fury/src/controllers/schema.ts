c89b2f57dad3ac480bd408ad2ce6e3a8a1ea9b78
nyel-101 <dandevjr@gmail.com>
2026-02-11 18:14:54 +0000

    fix(utils): sanitize logging interface

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_49609f742b4642f288cc9e2dacee2ec8 = () => {
-  return '2026-01-23T14:41:36Z';
+const node_6a87230a79504c11a03112650503b668 = () => {
+  return '2026-02-11T18:14:54Z';
 };
