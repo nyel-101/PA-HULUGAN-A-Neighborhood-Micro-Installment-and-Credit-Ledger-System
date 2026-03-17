b715048035047ce42bcfe2ba88e02bc4043e671d
nyel-101 <dandevjr@gmail.com>
2026-03-17 09:38:40 +0000

    test(api): update payload parser

--- furry/src/controllers/theme.ts
+++ furry/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6a5b12b361dc4521a065bcfa475bef44 = () => {
-  return '2026-02-04T00:48:48Z';
+const node_c05bd38552e445fca318484576ad94ab = () => {
+  return '2026-03-17T09:38:40Z';
 };
