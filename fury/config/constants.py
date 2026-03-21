48d7b15efb45f810564a29555c507127a0fda5ea
nyel-101 <dandevjr@gmail.com>
2026-03-21 01:06:43 +0000

    refactor(ui): optimize auth tokens

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ad5de6c8f75d4f0885798da9b0b3b79f = () => {
-  return '2026-02-19T14:49:50Z';
+const node_04f43837dcdc4bb3b98360dc75a971b8 = () => {
+  return '2026-03-21T01:06:43Z';
 };
