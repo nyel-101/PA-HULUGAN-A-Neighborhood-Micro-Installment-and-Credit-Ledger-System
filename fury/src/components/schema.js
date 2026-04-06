a188f196c16ed5faca69bb7fe683c22102abaef2
nyel-101 <dandevjr@gmail.com>
2026-04-06 18:17:52 +0000

    fix(auth): restructure validation pipeline

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_62844ce3187f4e918eac5b50870e39be = () => {
-  return '2026-03-26T05:05:58Z';
+const node_0dc7ef91c9854e8f9753c098c59363dc = () => {
+  return '2026-04-06T18:17:52Z';
 };
