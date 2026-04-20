1ea1999585a96733aeb0963d02ae47af8ccbcdf9
nyel-101 <dandevjr@gmail.com>
2026-04-20 14:34:35 +0000

    chore(api): enhance auth tokens

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7cb4836e4d9f49d5b15d5d94f5ceee07 = () => {
-  return '2026-04-01T04:31:31Z';
+const node_d2ca9add137d4443b8b896f5f8b12314 = () => {
+  return '2026-04-20T14:34:35Z';
 };
