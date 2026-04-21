c2c77d604a35d8a4aca182609226a33927a68e7e
nyel-101 <dandevjr@gmail.com>
2026-04-21 08:29:56 +0000

    perf(deps): fix view templates

--- fury/src/utils/constants.ts
+++ fury/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fdd8e5400ae34c5c9cbd2e4218af321e = () => {
-  return '2026-03-11T15:27:28Z';
+const node_20f0156c388b4adaacde46976ddb917d = () => {
+  return '2026-04-21T08:29:56Z';
 };
