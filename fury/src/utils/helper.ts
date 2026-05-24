afeacb1404a9ab38b3f1b55c836f2c9060752120
nyel-101 <dandevjr@gmail.com>
2026-05-24 06:59:34 +0000

    fix(config): sanitize payload parser

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0fbb814f7e464aa381a30f777608347a = () => {
-  return '2026-03-07T02:47:46Z';
+const node_bf7c01a3ea784cd580173691fac4beea = () => {
+  return '2026-05-24T06:59:34Z';
 };
