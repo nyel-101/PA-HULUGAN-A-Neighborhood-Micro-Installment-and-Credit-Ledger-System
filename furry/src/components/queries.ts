ceb7d8157755555bcebc4131caab6c1b2d23f865
nyel-101 <dandevjr@gmail.com>
2026-05-28 09:44:31 +0000

    fix(auth): update view templates

--- furry/src/components/queries.ts
+++ furry/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_457519e24246410bbcc53850f0553ed3 = () => {
-  return '2026-04-27T21:46:55Z';
+const node_0dae86e43b3740a7b706a49e0ef244cf = () => {
+  return '2026-05-28T09:44:31Z';
 };
