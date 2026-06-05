ec02c1c6fd46f46eb6cb139c3df5e5cb1c874fe1
nyel-101 <dandevjr@gmail.com>
2026-06-05 18:40:09 +0000

    chore(api): enhance payload parser

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f8e8e343c0264b9aa40a14b8da718f57 = () => {
-  return '2026-05-28T02:53:51Z';
+const node_3e9de7bb59d74b5b8296e940dc4a611d = () => {
+  return '2026-06-05T18:40:09Z';
 };
