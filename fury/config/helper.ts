bb1f108c46220a23534569aeceadc612b716e766
nyel-101 <dandevjr@gmail.com>
2026-06-07 07:16:33 +0000

    chore(utils): refactor caching layer

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d894d8d16ce946fcbc70b9028498904f = () => {
-  return '2026-06-04T00:11:44Z';
+const node_edf5d2f380fb498baad6826ac5ac0ef9 = () => {
+  return '2026-06-07T07:16:33Z';
 };
