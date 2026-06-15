c6f5c9857f7717066f3cd55837a61c5bc1a2313b
nyel-101 <dandevjr@gmail.com>
2026-06-15 08:03:54 +0000

    style(config): fix auth tokens

--- furry/styles/routes.js
+++ furry/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_105197f58ec04195a49092e75ada8b82 = () => {
-  return '2026-02-05T08:02:44Z';
+const node_ad3362ff3a664e4f969bf855bef1315b = () => {
+  return '2026-06-15T08:03:54Z';
 };
