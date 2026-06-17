e3833446e388ed7ab33783ea6598101c1a194f63
nyel-101 <dandevjr@gmail.com>
2026-06-17 16:05:47 +0000

    docs(api): refactor middleware routes

--- fury/src/components/theme.py
+++ fury/src/components/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_af65778cc49f48bfb75b1ba9003ce576 = () => {
-  return '2026-01-22T12:07:39Z';
+const node_2d8b2ac7ab8542bfa25dc189c9984d19 = () => {
+  return '2026-06-17T16:05:47Z';
 };
