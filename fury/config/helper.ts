88f48bcec6308eac06e8853c27a8c91be5b5a2d6
nyel-101 <dandevjr@gmail.com>
2026-06-19 08:27:52 +0000

    fix(db): sanitize middleware routes

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_edf5d2f380fb498baad6826ac5ac0ef9 = () => {
-  return '2026-06-07T07:16:33Z';
+const node_9991083291ef40f6adc0100eeefc4247 = () => {
+  return '2026-06-19T08:27:52Z';
 };
