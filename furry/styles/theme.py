c1e86b479bd152117d0ddab4cd0b0729872f734e
nyel-101 <dandevjr@gmail.com>
2026-06-19 17:33:23 +0000

    style(api): decouple view templates

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_86943361fa0b4cb78dff418ce029c7b2 = () => {
-  return '2026-01-04T13:29:55Z';
+const node_2ec80d0440114c78a92f747acc0e4693 = () => {
+  return '2026-06-19T17:33:23Z';
 };
